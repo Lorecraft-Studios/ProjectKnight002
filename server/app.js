9cc7954f6f0a2e949eae92f8d5a2911e443a5503
Raymond Luong <raymond.n.luong@gmail.com>
2016-12-21 17:38:11 -1000

    Serve /client folder through express

--- server/app.js
+++ server/app.js
@@ -1,6 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
 const World = require('./lib/World.js');
 const worldJSON = require('./snapshots/world_data_prologue_save.json');
+
 
 const myWorld = new World('prologue');
 myWorld.loadWorld(worldJSON);
 myWorld.getMapMatrix();
+
+
+
+//Serve static content
+app.set('port', 3000);
+app.use(express.static(path.join(__dirname, '/../client')));
+let server = app.listen(app.get('port'), () => {
+  let port  = server.address().port;
+  console.log('Serving on port ' + port);
+})
