052e6ad45ee7accc7cbc0b64aa8ecfdcc0f674c7
Raymond Luong <raymond.n.luong@gmail.com>
2016-12-22 09:26:33 -1000

    Tweak css

--- client/js/client_app.js
+++ client/js/client_app.js
@@ -1,1 +1,45 @@
 console.log('Hello World from client_app.js');
+
+let cy = cytoscape({
+  container: document.getElementById('cy'),
+  elements: [ // list of graph elements to start with
+    { // node a
+      data: { id: 'a' }
+    },
+    { // node b
+      data: { id: 'b' }
+    },
+    { // edge ab
+      data: { id: 'ab', source: 'a', target: 'b' }
+    },
+    { // edge ba
+      data: { id: 'ba', source: 'b', target: 'a' }
+    }
+
+  ],
+  style: [ // the stylesheet for the graph
+    {
+      selector: 'node',
+      style: {
+        'background-color': '#ccc',
+        'label': 'data(id)'
+      }
+    },
+    {
+      selector: 'edge',
+      style: {
+        'curve-style': 'bezier',
+        'width': 3,
+        'line-color': '#666',
+        'target-arrow-color': '#666',
+        'target-arrow-shape': 'triangle'
+      }
+    }
+  ],
+  layout: {
+    name: 'grid',
+    rows: 1
+  }
+});
+
+console.log('***cy***', cy)
